4158be335dfc23085372f2ed583bf5312271f76b
HFStudio-faryou <143365907+HFStudio-faryou@users.noreply.github.com>
2025-11-07 19:31:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="zh-CN">
   <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href=css/default.css>
     <title>faryou的博客 - 平平淡淡 从从容容才是真</title>
   </head>
   <body>
